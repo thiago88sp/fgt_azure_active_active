e4f43978585c19d2aed2a0e2737ec8c1e6fe3bb5
Thiago Pontes <tpontes@fortinet.com>
2024-03-14 17:58:36 -0300

    Update Output.tf

--- output.tf
+++ output.tf
@@ -1,31 +1,26 @@
 output "ResourceGroup" {
   value = azurerm_resource_group.res-0.name
+  description = "Resource Group where the FortiGates are placed"
 }
-
-#output "ActiveMGMTPublicIP" {
-#  value       = format("https://%s:%s", azurerm_public_ip.ActiveMGMTIP.ip_address, var.adminsport)
-#  description = "FGT-A-MGMT-PIP"
-#}
-
-#output "PassiveMGMTPublicIP" {
-#  value       = format("https://%s:%s", azurerm_public_ip.PassiveMGMTIP.ip_address, var.adminsport)
-#  description = "FGT-B-MGMT-PIP"
-#}
 
 output "FrontendIP-ELB" {
   value = azurerm_public_ip.ClusterPublicIP.ip_address
+  description = "External Load Balancer public IP."
 
 }
 
 output "FrontendIP-ILB" {
   value = azurerm_lb.internalLB.private_ip_address
+  description = "Internal Load Balancer private IP."
 
 }
 
 output "Username" {
   value = var.admin_username
+  description = "Fortigate access user"
 }
 
 output "Password" {
   value = var.admin_password
+  description = "Fortigate access password"
 }
